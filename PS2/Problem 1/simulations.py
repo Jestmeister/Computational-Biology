57db6eb9c2a2451fb4288623dbfd69193bfafc08
Jestmeister <63909272+Jestmeister@users.noreply.github.com>
2022-02-18 12:45:07 +0100

    Update simulations.py

--- PS2/Problem 1/simulations.py
+++ PS2/Problem 1/simulations.py
@@ -9,13 +9,15 @@ if __name__ == '__main__':
     #given
     rho = 0.5
     q = 8
+    u_star_1 = (q-1)/2 + np.sqrt((q-1)**2/4 + q*(1-1/rho))  #Largest steady-state
+    u_star_2 = (q-1)/2 - np.sqrt((q-1)**2/4 + q*(1-1/rho))  #2nd Largest steady-state
     L = 100
-    u0 = 50    #CHANGE
+    u0 = u_star_1
     x0 = 20
 
     #user defined
     dL = 1
-    T = 10
+    T = 100
     dt = 0.1
     steps = int(T/dt)
 
@@ -31,8 +33,11 @@ if __name__ == '__main__':
         for i in range(1,L-1):
             u[i,t+1] = u[i,t] + dt*(rho*u[i,t]*(1 - u[i,t]/q) - u[i,t]/(1 + u[i,t]) + (u[i+1,t] + u[i-1,t] - 2*u[i,t])/dL**2)
 
-    #plt.plot(u[:,0])
-    #plt.plot(u[:,steps-1])
+    plt.figure()
+    plt.plot(u[:,0])
+    plt.figure()
+    plt.plot(u[:,steps-1])
+    plt.figure()
     plt.imshow(u, cmap='hot')
     plt.colorbar(orientation='horizontal')
     plt.show()
